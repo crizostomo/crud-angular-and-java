69f60ffd155ff7872470be675c0968441016c0bd
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-21 17:27:02 -0300

    Angular + Spring | 21: Form: Spring API: Create Course (HTTP POST)

--- spring-crud/src/main/java/com/dev/springcrud/controller/CourseController.java
+++ spring-crud/src/main/java/com/dev/springcrud/controller/CourseController.java
@@ -3,9 +3,9 @@ package com.dev.springcrud.controller;
 import com.dev.springcrud.model.Course;
 import com.dev.springcrud.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +21,8 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    }
 }
