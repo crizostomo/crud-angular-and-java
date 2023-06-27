1d97316ca698a4dbd413eea6efb85630bd05faeb
crizostomo <diogo_crizostomo@hotmail.com>
2023-06-27 15:11:55 -0300

    Angular + Spring | 29: Update Course - Saving the form

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -27,9 +27,27 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+   // console.log(record);
+    if (record._id) {
+      //console.log('update');
+      return this.update(record);
+    }
+    //console.log('create');
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record)
     .pipe(
       take(1)
       );
   }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record)
+    .pipe(
+      take(1)
+      );
+  }
+
 }
