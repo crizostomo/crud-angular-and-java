01a625091ef86a00e0707a5ff5587f91f086428a
crizostomo <diogo_crizostomo@hotmail.com>
2023-07-02 20:04:47 -0300

    Angular + Spring | 40: Handling exception by using Controller Advice

--- spring-crud/src/main/java/com/dev/springcrud/controller/CourseController.java
+++ spring-crud/src/main/java/com/dev/springcrud/controller/CourseController.java
@@ -1,17 +1,15 @@
 package com.dev.springcrud.controller;
 
 import com.dev.springcrud.model.Course;
-import com.dev.springcrud.repository.CourseRepository;
 import com.dev.springcrud.service.CourseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Validated
@@ -28,29 +26,24 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseService.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
+    public Course findById(@PathVariable @NotNull @Positive Long id) {
+        return courseService.findById(id);
     }
 
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody @Valid Course course) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.create(course));
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody @Valid Course course) {
+        return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
-        return courseService.update(id, course)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
+    public Course update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
+        return courseService.update(id, course);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-        if (courseService.delete(id)) {
-            return ResponseEntity.noContent().<Void>build();
-        }
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+        courseService.delete(id);
     }
 }
--- spring-crud/src/main/java/com/dev/springcrud/service/CourseService.java
+++ spring-crud/src/main/java/com/dev/springcrud/service/CourseService.java
@@ -1,19 +1,17 @@
 package com.dev.springcrud.service;
 
+import com.dev.springcrud.exception.RecordNotFoundException;
 import com.dev.springcrud.model.Course;
 import com.dev.springcrud.repository.CourseRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 
 @Validated
 @Service
@@ -30,29 +28,30 @@ public class CourseService {
         return courseRepository.findAll();
     }
 
-    public Optional<Course> findById(@NotNull @Positive Long id) {
-        return courseRepository.findById(id);
+    public Course findById(@NotNull @Positive Long id) {
+        return courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     public Course create(@Valid Course course) {
         return courseRepository.save(course);
     }
 
-    public Optional<Course> update(@NotNull @Positive Long id, @Valid Course course) {
+    public Course update(@NotNull @Positive Long id, @Valid Course course) {
         return courseRepository.findById(id)
                 .map(record -> {
                     record.setName(course.getName());
                     record.setCategory(course.getCategory());
                     return courseRepository.save(record);
-                });
+                }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public boolean delete(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-                .map(record -> {
-                    courseRepository.deleteById(id);
-                    return true;
-                })
-                .orElse(false);
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+        courseRepository.delete(courseRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(id)));
+//        courseRepository.findById(id) // Another option to do the same thing
+//                .map(record -> {
+//                    courseRepository.deleteById(id);
+//                    return true;
+//                })
+//                .orElseThrow(() -> new RecordNotFoundException(id));
     }
 }
