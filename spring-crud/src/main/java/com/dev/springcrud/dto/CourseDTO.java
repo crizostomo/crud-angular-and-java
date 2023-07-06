186eec5a94f59f5a625438d54b41ba2e819e772c
crizostomo <diogo_crizostomo@hotmail.com>
2023-07-05 21:24:49 -0300

    Angular + Spring | 47: Lessons-Course: DTO Lesson

--- spring-crud/src/main/java/com/dev/springcrud/dto/CourseDTO.java
+++ spring-crud/src/main/java/com/dev/springcrud/dto/CourseDTO.java
@@ -1,6 +1,5 @@
 package com.dev.springcrud.dto;
 
-import com.dev.springcrud.model.Lesson;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -12,7 +11,7 @@ public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotNull @Length(min = 4, max = 100) String name,
         @NotNull @Length(max = 10) @Pattern(regexp = "Back-End|Front-End") String category,
-        List<Lesson> lessons
+        List<LessonDTO> lessons
 //        @NotBlank @NotNull @Length(max = 10)  @Pattern(regexp = "Active|Inactive") String status // We will not expose this data
 ) {
 }
---
+++ spring-crud/src/main/java/com/dev/springcrud/dto/LessonDTO.java
@@ -0,0 +1,8 @@
+package com.dev.springcrud.dto;
+
+public record LessonDTO(
+        Long id,
+        String name,
+        String shareableYoutubeUrl
+) {
+}
