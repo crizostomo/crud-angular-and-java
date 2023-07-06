d08ddb875e050f3539626a9a308d5e9297bcf640
crizostomo <diogo_crizostomo@hotmail.com>
2023-07-06 12:47:22 -0300

    Angular + Spring | 48: Lessons-Course: Lessons Interface

--- crud-angular/src/app/courses/model/course.ts
+++ crud-angular/src/app/courses/model/course.ts
@@ -1,5 +1,8 @@
+import { Lesson } from "./lesson";
+
 export interface Course {
   _id: string;
   name: string;
   category: string;
+  lessons: Lesson[]; // We need 'lessons:?' to run the app even if we have empty lessons
 }
